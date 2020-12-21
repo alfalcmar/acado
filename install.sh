77477c78758a76e689b4cc0b00a6d9e0c40d3fd5
Vit Kratky <kratkvit@fel.cvut.cz>
2020-12-21 15:22:38 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -6,6 +6,20 @@ cd $SCRIPT_PATH/build
 cmake ..
 make
 
-# set bashrc
-echo "
-source $SCRIPT_PATH/build/acado_env.sh" >> ~/.bashrc
+
+# set bashrc/zshrc
+if [ $SHELL = /usr/bin/zsh ]; then
+SHELL_TYPE=zsh
+elif [ $SHELL = /bin/bash ]; then 
+SHELL_TYPE=bash
+fi
+
+RCFILE=.${SHELL_TYPE}rc
+
+num=`cat ~/$RCFILE | grep "acado_env.sh" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  echo "adding sourcing of workspace to rc file"
+  echo "source $SCRIPT_PATH/build/acado_env.sh" >> ~/$RCFILE
+  
+fi
