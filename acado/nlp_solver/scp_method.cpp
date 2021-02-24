f583fc8aaed9f4b7f756ba2d702604d365f2d1bf
alfalcmar <a.alcantara.marin@gmail.com>
2021-02-24 10:08:52 +0100

    avoiding prints

--- acado/nlp_solver/scp_method.cpp
+++ acado/nlp_solver/scp_method.cpp
@@ -289,7 +289,7 @@ returnValue SCPmethod::solve(	const DVector &x0_,
 		if( returnvalue != CONVERGENCE_NOT_YET_ACHIEVED )
 			return ACADOERROR( RET_NLP_SOLUTION_FAILED );
 		
-		std::cout<<"clock: "<<solver_clock.getTime()<<"max time: "<<max_time<<std::endl;
+		// std::cout<<"clock: "<<solver_clock.getTime()<<"max time: "<<max_time<<std::endl;
 		if( max_time<solver_clock.getTime())
 			return ACADOERROR( RET_MAX_TIME_REACHED);
 	}
